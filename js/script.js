fb3207864ef5159d55c4ed99daff8ff3fddc49e5
ksubondar <79761397+ksubondar@users.noreply.github.com>
2021-05-04 21:12:54 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        nextArrow: '<button class="slider-arrow arrow-next"><img src="../img/slider-next.svg" alt="next arrow"></button>',
+        prevArrow: '<button class="slider-arrow arrow-prev"><img src="../img/slider-prev.svg" alt="prev arrow"></button>',
+
+        responsive: [
+            {
+                breakpoint: 441,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+
+
+    });
+
+
+});
